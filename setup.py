033364c8d561d4f60dbe131b5153f669c69054a1
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2025-05-04 10:47:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ht-serve",
-    version="0.0.1",
+    version="0.0.2",
     description="Secure HTTPS live reload server for UI/dashboard development",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
